b9e277d603bb7ef60bd3f5ec55017914ebffd701
KavitaNeg123 <kavita@codalien.com>
2022-12-10 19:20:34 +0530

    ui fixes

--- src/components/popover/index.js
+++ src/components/popover/index.js
@@ -6,8 +6,14 @@ function PopOver() {
 
         <div className="modal-container">
             <div className="popup">
-                <div>
-                    hello
+                <div className="container">
+                   <div className="row">
+                       <div className="col-5">
+                           <div className = "experts-call-section"><h6 class="size">Call Our experts</h6></div>
+                       </div>
+                       <div className="col-2"></div>
+                       <div className="col-5"></div>
+                   </div>
                 </div>
             </div>
         </div>
