7203e99433f1b76fa5e2b57b88695fc9f4572a19
KavitaNeg123 <kavita@codalien.com>
2022-12-10 20:22:56 +0530

    ui fixes

--- src/components/popover/index.js
+++ src/components/popover/index.js
@@ -1,23 +1,57 @@
 import React from "react";
 import "./style.css"
+import CheckList from "../../assets/img/checklist.png"
 
 function PopOver() {
-  return (
+    return (
 
         <div className="modal-container">
             <div className="popup">
                 <div className="container">
-                   <div className="row">
-                       <div className="col-5">
-                           <div className = "experts-call-section"><h6 class="size">Call Our experts</h6></div>
-                       </div>
-                       <div className="col-2"></div>
-                       <div className="col-5"></div>
-                   </div>
+                    <div className="row">
+                        <div className="col-5">
+                            <div className="experts-call-section">
+                                <h6 class="size">Call Our experts</h6>
+                                <h5 className="data">Data Science: +91 7063119228</h5>
+                                <h5 className="data">Product Management: +91 9625811095</h5>
+                                <div className="checklist-container"><img src={CheckList}/></div>
+                            </div>
+                        </div>
+                        <div className="col-1 border-pipe">
+                        <span class="sepText">
+                              OR
+                            </span> 
+                        </div>
+                        <div className="col-5">
+                            <div className="ScheduleConsultation">
+                            <h6 class="size">Schedule a 1-on-1 consultation</h6> 
+                            <h5 className="data callBack-request">Request a Call Back</h5>
+                             <form>
+                                 <div className="form-group">
+                                     <input type="text" placeholder="Mobile"/>
+                                 </div>
+                                 <div className="form-group">
+                                     <input type="text" placeholder="Name"/>
+                                 </div>
+                                 <div className="form-group">
+                                     <input type="text" placeholder="Email"/>
+                                 </div>
+                                 <div className="form-group">
+                                     <select>
+                                         <option>---- Select Program ----</option>
+                                         <option>Data Science</option>
+                                         <option>Product Management</option>
+                                     </select>
+                                 </div>
+                                 <input type="submit" value="Call Me Back" class="submit-btn" id="btncolor"></input>
+                             </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-  );
+    );
 }
 
 export default PopOver;
