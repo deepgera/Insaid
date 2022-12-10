76a0a3f706bcfce0ddb340d75304090e7b0ce815
deepakgera <deepak.gera@hudini.io>
2022-12-10 18:54:49 +0530

    popover

--- src/components/jobSection/index.js
+++ src/components/jobSection/index.js
@@ -1,32 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
+import PopOver from "../popover";
+
 
 function Jobs() {
-    return (
-        <div className="job-container">
-            <div className="container">
-                <div class="row align-items-center">
-                    <div className="col-6">
-                        <section className="row mt-5 align-items-center">
-                            <div className="col-12 ms-4 mt-5 pt-2 sec">
-                                <h4 className="getjob">Get your Dream Job with </h4>
-                                <h2 className="mb-0 programs"><b>Top Programs</b></h2>
-                                <div className="col-12 mx-2 my-4 sec">
-                                    <p  className="description-content"> Learn with India's leading online school for working professionals </p>
-                                </div>
-                                <div className="col-12 mx-2 d-flex justify-content-left">
-                                    <button className ="btn-lg explore" data-toggle="modal" data-target="#book_class"><b>Talk to an Expert</b></button>
-                            </div>
-                            </div>
-                        </section>
-                    </div>
-                    <div className="col-6">
-
-                    </div>
+    const [showPopover,setshowPopOver]=useState(false);
+   function ShowPopover(){
+    console.log("popover")
+    setshowPopOver(true)
+   }
+  return (
+    <div className="job-container">
+      <div className="container">
+        <div class="row align-items-center">
+          <div className="col-6">
+            <section className="row mt-5 align-items-center">
+              <div className="col-12 ms-4 mt-5 pt-2 sec">
+                <h4 className="getjob">Get your Dream Job with </h4>
+                <h2 className="mb-0 programs">
+                  <b>Top Programs</b>
+                </h2>
+                <div className="col-12 mx-2 my-4 sec">
+                  <p className="description-content">
+                    {" "}
+                    Learn with India's leading online school for working
+                    professionals{" "}
+                  </p>
                 </div>
-            </div>
+                <div className="col-12 mx-2 d-flex justify-content-left">
+                      <button
+                        onClick={ShowPopover}
+                        className="btn-lg explore"
+                        data-toggle="modal"
+                        data-target="#book_class"
+                      >
+                        <b>Talk to an Expert</b>
+                        
+                      </button>
+                </div>
+              </div>
+            </section>
+          </div>
+          <div className="col-6"></div>
         </div>
-    );
+      </div>
+      {showPopover==true?
+        <PopOver/>:
+        <p></p>
+        }
+    </div>
+  );
 }
 
 export default Jobs;
---
+++ src/components/popover/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css"
+
+function PopOver() {
+  return (
+
+        <div className="modal-container">
+            <div className="popup">
+                <div>
+                    hello
+                </div>
+            </div>
+        </div>
+  );
+}
+
+export default PopOver;
